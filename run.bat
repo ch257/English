5ecb89ffc59655d6be50f7e2059ab66945f5fc35
a_ch <a_ch@ch.ch>
2017-04-06 00:44:32 +0400

    2

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python scripts/tst.py
+python scripts/tst.py settings.ini
 
 @echo off
 set /P pe=Press Enter! 
--- scripts/tst.py
+++ scripts/tst.py
@@ -1,5 +1,25 @@
-def hello():
-	'''say hello'''
-	print('hello!')
+# -*- coding: utf-8 -*- 
+import sys
 
-hello()
+class run:
+	def __init__(self):
+		self.err = False
+		self.err_desc = ""
+		
+	def rise_err(self, method_name, err_desc):
+		self.err = True
+		self.err_desc = "\n  Error in '" + self.__class__.__name__ + "." + method_name + "':" + err_desc + "\n"
+
+	def read_ini(self, ini_file):
+		pass
+
+	def main(self, arguments):
+		if len(arguments) > 1:
+			print(arguments)
+		else:
+			self.rise_err(sys._getframe().f_code.co_name, "ini file not present")
+
+r = run()
+r.main(sys.argv)
+if r.err:
+	print(r.err_desc)

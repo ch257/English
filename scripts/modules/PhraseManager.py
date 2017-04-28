da9a09239ea3c40152562ebe0a7add38421ff8bf
home <home@home.home>
2017-04-28 22:31:12 +0500

    phraser continue 002

--- scripts/modules/PhraseManager.py
+++ scripts/modules/PhraseManager.py
@@ -16,15 +16,32 @@ class PhraseManager:
 
 	def find_phrase(self, work_folder, text_file_name):
 		if not self.err:
+			excluded_symbols = (chr(32), '\n', '\t', '?', '!', '.', ',', '"')
 			text_file = RWFile(work_folder, text_file_name, 'read', 'utf-8')
+			word = ''
+			words = []
+			words_satarted = False
 			while not text_file.err:
-				smbs = text_file.read_symbols(10)
-				if smbs:
-					print(smbs)
+				smb = text_file.read_symbols(1)
+				if smb:
+					if smb in excluded_symbols:
+						if words_satarted:
+							words.append(word.lower())
+							word = ''
+							words_satarted = False
+					else:
+						if words_satarted:
+							word += smb
+						else:
+							word += smb
+							words_satarted = True
+					
 				else:
 					break
 			
 			text_file.close_file()
 
+			print(words)
+			
 			if text_file.err:
 				self.rise_err(sys._getframe().f_code.co_name, text_file.err_desc)

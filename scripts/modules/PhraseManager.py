a8854b7f19ec239980da1272b1706b65d9c94e75
a_ch <a_ch@ch.ch>
2017-05-04 19:20:14 +0500

    reassemling 01

--- scripts/modules/PhraseManager.py
+++ scripts/modules/PhraseManager.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*- 
 from modules.RWFile import *
+from modules.TextTools import *
 from modules.AudioTools import *
 
 class PhraseManager:
-	def __init__(self):
+	def __init__(self, cfg):
 		self.err = False
 		self.err_desc = ''
 		
+		self.cfg = cfg
 		self.text_words = {}
 		self.text_labels = {}
 		self.text_source_words = {}
 		self.time_labels = {}
+		self.text_tools = TextTools()
 		self.audio_tools = AudioTools()
 
 	def rise_err(self, method_name, err_desc):
@@ -37,22 +40,29 @@ class PhraseManager:
 			words_cnt += 1
 		return phrase
 	
-	def create_media(self, work_folder, audio_file_name, output_folder, phrase_start, phrase_end):
+	def create_media(self, phrase_start, phrase_end):
 		print(self.glue_phrase(phrase_start, phrase_end))
 		start_time = self.time_labels[phrase_start]
 		end_time = self.time_labels[phrase_end]
 		
-		self.audio_tools.cut_sample(work_folder, audio_file_name, output_folder, start_time, end_time)
+		# self.audio_tools.cut_sample(work_folder, audio_file_name, output_folder, start_time, end_time)
 	
-	def find_phrase(self, work_folder, audio_file_name, output_folder, mp3splt_folder):
+	def find_phrase(self):
 		if not self.err:
+			self.text_tools.find_text_words(self.cfg)
+			self.text_tools.find_time_labels(self.cfg)
+			
+			self.text_words = self.text_tools.text_words
+			self.text_source_words = self.text_tools.text_source_words
+			self.time_labels = self.text_tools.time_labels
+			
 			# self.text_words = {0:'', 1:'', 2:'', 3:'', 4:'', 5:''}
 			words_cnt = 0
 			phrase_start = words_cnt
 			phrase_end = phrase_start + 1
 			phrase = self.text_words[words_cnt]
 			while True:
-				self.create_media(work_folder, audio_file_name, output_folder, phrase_start, phrase_end)
+				self.create_media(phrase_start, phrase_end)
 				manage_seq = input()
 				action, offset = self.determine_action(manage_seq)
 				
@@ -77,7 +87,7 @@ class PhraseManager:
 						phrase_end = len(self.text_words) - 1
 					
 				elif action == '--':
-					if phrase_start - offset > 1:
+					if phrase_start - offset > 0:
 						phrase_start -= offset
 						phrase_end -= offset
 					else:
--- scripts/phraser.py
+++ scripts/phraser.py
@@ -2,7 +2,7 @@
 import sys
 import configparser
 from modules.RWFile import *
-from modules.TextTools import *
+from modules.PhraseManager import *
 
 class MainProgram:
 	def __init__(self):
@@ -33,75 +33,6 @@ class MainProgram:
 				self.rise_err(sys._getframe().f_code.co_name, ini_file.err_desc)
 			ini_file.close_file()
 
-	def determine_action(self, manage_seq):
-		offset = ''
-		action = ''
-		for smb in manage_seq:
-			if ord(smb) > 47 and ord(smb)< 58:
-				offset += smb
-			else:
-				action += smb
-		if not offset:
-			offset = '1'
-		return action, int(offset)
-		
-	def glue_phrase(self, phrase_start, phrase_end):
-		words_cnt = phrase_start
-		phrase = ''
-		while words_cnt < phrase_end:
-			phrase += self.text_source_words[words_cnt]
-			words_cnt += 1
-		return phrase
-	
-	def create_media(self, phrase_start, phrase_end):
-		# print(phrase_start, phrase_end)
-		print(self.glue_phrase(phrase_start, phrase_end))
-		start_time = self.time_labels[phrase_start]
-		end_time = self.time_labels[phrase_end]
-		
-	def find_phrase(self):
-		if not self.err:
-			# self.text_words = {0:'', 1:'', 2:'', 3:'', 4:'', 5:''}
-			words_cnt = 0
-			phrase_start = words_cnt
-			phrase_end = phrase_start + 1
-			while True:
-				self.create_media(phrase_start, phrase_end)
-				
-				manage_seq = input()
-				action, offset = self.determine_action(manage_seq)
-				
-				if action == '+':
-					if phrase_end + offset < len(self.text_words):
-						phrase_end += offset
-					else:
-						phrase_end = len(self.text_words) - 1
-				
-				elif action == '-':
-					if phrase_end - offset > 1:
-						phrase_end -= offset
-					else:
-						phrase_end = 1
-				
-				elif action == '++':
-					if phrase_end + offset < len(self.text_words):
-						phrase_start += offset
-						phrase_end += offset
-					else:
-						phrase_start = len(self.text_words) + phrase_start - phrase_end - 1
-						phrase_end = len(self.text_words) - 1
-					
-				elif action == '--':
-					if phrase_start - offset > 1:
-						phrase_start -= offset
-						phrase_end -= offset
-					else:
-						phrase_end = - phrase_start + phrase_end
-						phrase_start = 0
-				
-				elif action == 'q':
-					break
-	
 	def main(self, arguments):
 		self.read_cfg(arguments)
 		if not self.err:
@@ -110,18 +41,14 @@ class MainProgram:
 			# = self.cfg['params']['']
 			# = self.cfg['path']['']
 
-			text_tools = TextTools()
-			text_tools.find_text_words(self.cfg)
-			text_tools.find_time_labels(self.cfg)
-			self.text_words = text_tools.text_words
-			self.text_source_words = text_tools.text_source_words
-			self.time_labels = text_tools.time_labels
-			self.find_phrase()
+			phrase_manger = PhraseManager(self.cfg)
+
+			phrase_manger.find_phrase()
 			
 			# for words_cnt in text_tools.text_words:
 				# print(text_tools.text_words[words_cnt], text_tools.text_labels[words_cnt], text_tools.time_labels.get(words_cnt), text_tools.text_source_words[words_cnt])
 			
-			if (text_tools.err):
+			if (phrase_manger.err):
 				self.rise_err(sys._getframe().f_code.co_name, text_tools.err_desc)
 			# ---------------------------------------------
 			print("OK")
